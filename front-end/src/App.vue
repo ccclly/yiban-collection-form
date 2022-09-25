996d126327f460326cec14a5e818f104ec5d14ca
ccclly <1434046860@qq.com>
2022-09-25 16:36:04 +0800

    前端页面编写

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CollectionForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CollectionForm from './components/CollectionForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CollectionForm
   }
 }
 </script>
@@ -21,6 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+
+// 引入element ui
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+// 引入字体文件
+import './assets/fonts/font.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
