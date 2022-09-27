26b8e4fa509e4f4d93c2d33f736a31d3e0bd9ce2
ccclly <1434046860@qq.com>
2022-09-27 13:00:08 +0800

    #

--- front-end/src/components/CollectionForm.vue
+++ front-end/src/components/CollectionForm.vue
@@ -137,12 +137,12 @@ export default {
         if (valid) {
           sentMessage(this.form).then(
               response => {
-                if (response.type === 'ok') {
+                if (response.data.type === 'ok') {
                   ElMessage({
                     message: '提交成功',
                     type: 'success',
                   });
-                } else if (response.type === 'repeat') {
+                } else if (response.data.type === 'repeat') {
                   ElMessage.error('重复提交');
                 }
                 console.log(response);
